0179681d345471131e80d625aa861770e64b4ce2
Sasha <sasha@weibyapps.com>
2021-05-19 01:47:00 +0800

    v-for prac

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <ul v-for="(list, index) in lists" :key="index">
+    <h1>{{ list.title }}</h1>
+    <li v-for="(item, index) in list.contents" :key="index">
+      {{ item }}
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  delimiters: ["${", "}", "."],
+  data() {
+    return {
+      lists: [
+        {
+          title: "All",
+          contents: [
+            ".vue",
+            "{ { } }",
+            "data",
+            "v-bind",
+            "v-for",
+            "computed",
+            "v-if",
+            "v-show",
+            "v-model",
+            "v-on",
+            "methods",
+            "props",
+            "typed-props",
+            "mounted",
+            "axios",
+          ],
+        },
+        {
+          title: "First ( first 6 items )",
+          contents: [".vue", "{ { } }", "data", "v-bind", "v-for", "computed"],
+        },
+        {
+          title: "Second ( 7th ~)",
+          contents: [
+            "v-if",
+            "v-show",
+            "v-model",
+            "v-on",
+            "methods",
+            "props",
+            "typed-props",
+            "mounted",
+          ],
+        },
+      ],
+    };
   },
 };
 </script>
@@ -19,7 +62,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
 }
